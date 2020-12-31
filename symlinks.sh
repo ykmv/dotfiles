6743f12dbf4b3e2ea845cb89e748372a1122d41a
ykmv <yakumov64@gmail.com>
2020-12-31 15:12:19 +0700

    Added scripts to make links

---
+++ symlinks.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+ln -s ~/dotfiles/.Xresources ~/.Xresources
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.scripts ~/.scripts
+ln -s ~/dotfiles/.mkshrc ~/.mkshrc
+ln -s ~/dotfiles/.config/dunst ~/.config/dunst
+ln -s ~/dotfiles/.config/lf ~/.config/lf
+ln -s ~/dotfiles/.config/picom ~/.config/picom
+ln -s ~/dotfiles/.config/vim ~/.config/vim
+ln -s ~/dotfiles/.config/newsboat ~/.config/newsboat
+ln -s ~/dotfiles/.config/openbox ~/.config/openbox
